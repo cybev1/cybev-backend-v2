75885f290fe5e95fd64a47c1384bae9df6b459a8
cybev1 <cybev.io@gmail.com>
2025-05-23 12:07:57 -0700

    Update post.routes.js

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const { createPost, getUserPosts, getPublicPosts, getPostById } = require('../controllers/post.controller');
-const auth = require('../middleware/auth.middleware');
 const router = express.Router();
+const { createPost } = require('../controllers/post.controller');
+const auth = require('../middleware/auth.middleware');
 
 router.post('/create', auth, createPost);
-router.get('/user', auth, getUserPosts);
-router.get('/', getPublicPosts);
-router.get('/:id', getPostById);
-
 module.exports = router;
