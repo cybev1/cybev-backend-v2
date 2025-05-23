e09c56269fb17ca6bd76d748e55210a8d52f9a49
cybev1 <cybev.io@gmail.com>
2025-05-23 12:09:26 -0700

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,43 +1,8 @@
-const Post = require('../models/post.model');
-
 exports.createPost = async (req, res) => {
+  const { title, category, content } = req.body;
   try {
-    const { title, category, content } = req.body;
-    const newPost = await Post.create({
-      title,
-      category,
-      content,
-      userId: req.userId
-    });
-    res.status(201).json(newPost);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-exports.getUserPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({ userId: req.userId }).sort({ createdAt: -1 });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-exports.getPublicPosts = async (req, res) => {
-  try {
-    const posts = await Post.find().sort({ createdAt: -1 });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-exports.getPostById = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) return res.status(404).json({ message: 'Post not found' });
-    res.json(post);
+    const post = await Post.create({ title, category, content, userId: req.userId });
+    res.status(201).json(post);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
