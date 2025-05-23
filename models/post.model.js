6ae9e5ad0775cfaf57a9cbd838046b0201154af2
cybev1 <cybev.io@gmail.com>
2025-05-23 12:11:24 -0700

    Update post.model.js

--- models/post.model.js
+++ models/post.model.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type: String },
-  content: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  views: { type: Number, default: 0 },
-  likes: { type: Number, default: 0 },
-  comments: { type: Number, default: 0 },
-  isMinted: { type: Boolean, default: false }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Post', postSchema);
+const Post = require('../models/post.model');
