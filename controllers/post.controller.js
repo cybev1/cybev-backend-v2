68c40dbb03f0cf9e27cc94a98c1d4aefe2e028a5
cybev1 <cybev.io@gmail.com>
2025-05-23 12:18:36 -0700

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,7 +1,14 @@
+const Post = require('../models/post.model');
+
 exports.createPost = async (req, res) => {
   const { title, category, content } = req.body;
   try {
-    const post = await Post.create({ title, category, content, userId: req.userId });
+    const post = await Post.create({
+      title,
+      category,
+      content,
+      userId: req.userId // Automatically from auth middleware
+    });
     res.status(201).json(post);
   } catch (err) {
     res.status(500).json({ message: err.message });
