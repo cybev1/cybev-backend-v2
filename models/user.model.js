57414b4b56b3ed44d8c0ffdb9165f56d09036168
cybev1 <cybev.io@gmail.com>
2025-05-25 14:17:25 -0700

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  referrer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
