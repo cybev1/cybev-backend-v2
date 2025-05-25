5aee2095ba2f4b3a77a525475d7c9758561f7be3
cybev1 <cybev.io@gmail.com>
2025-05-24 20:05:04 -0700

    Add files via upload

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,10 +1,9 @@
 const Blog = require('../models/blog.model');
 
-exports.deleteBlog = async (req, res) => {
+exports.getUserBlogs = async (req, res) => {
   try {
-    const blog = await Blog.findOneAndDelete({ _id: req.params.id, userId: req.userId });
-    if (!blog) return res.status(404).json({ message: 'Blog not found or unauthorized' });
-    res.json({ message: 'Blog deleted successfully' });
+    const blogs = await Blog.find({ userId: req.userId }).sort({ createdAt: -1 });
+    res.json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
