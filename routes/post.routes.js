ce441d17eec46ad72719304852d38a52ae9188a6
cybev1 <cybev.io@gmail.com>
2025-05-26 05:18:24 -0700

    Update post.routes.js

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { createPost } = require('../controllers/post.controller');
 const auth = require('../middleware/auth.middleware');
 
+// ✅ Only logged-in users can create posts
 router.post('/create', auth, createPost);
 
 module.exports = router;
