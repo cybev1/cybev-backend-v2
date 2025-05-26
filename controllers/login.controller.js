b924b0108e7a559d542b3edd16fa636c36b1349f
cybev1 <cybev.io@gmail.com>
2025-05-26 02:41:09 -0700

    Update login.controller.js

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,7 +5,11 @@ const jwt = require('jsonwebtoken');
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+
+    // ✅ Allow login with either email or username
+    const user = await User.findOne({
+      $or: [{ email: email }, { username: email }]
+    });
 
     if (!user) return res.status(401).json({ message: 'Invalid credentials' });
 
@@ -14,7 +18,10 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || 'cybev', { expiresIn: '7d' });
 
-    res.status(200).json({ token, user: { username: user.username, email: user.email } });
+    res.status(200).json({
+      token,
+      user: { username: user.username, email: user.email }
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
