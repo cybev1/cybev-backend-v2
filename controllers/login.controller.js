9e69bc075c4592a9011c0cb2fb88d6a04fa274a0
cybev1 <cybev.io@gmail.com>
2025-05-26 00:09:20 -0700

    Add files via upload

---
+++ controllers/login.controller.js
@@ -0,0 +1,21 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || 'cybev', { expiresIn: '7d' });
+
+    res.status(200).json({ token, user: { username: user.username, email: user.email } });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
