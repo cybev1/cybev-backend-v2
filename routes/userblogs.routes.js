765117d2394fa3189fea2e52fc5aca51719313ad
cybev1 <cybev.io@gmail.com>
2025-05-26 04:53:24 -0700

    Update userblogs.routes.js

--- routes/userblogs.routes.js
+++ routes/userblogs.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getUserBlogs } = require('../controllers/blog.controller');
 const auth = require('../middleware/auth.middleware');
+
 const router = express.Router();
 
+// ✅ Protect user-specific blog fetch
 router.get('/blogs/user', auth, getUserBlogs);
 
 module.exports = router;
