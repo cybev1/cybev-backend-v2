7ddedd0d7a32d0ccd92ce253cd63a4574cf1e600
cybev1 <cybev.io@gmail.com>
2025-05-26 05:20:29 -0700

    Update blog.routes.js

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
 const { createBlog, getBlogBySubdomain } = require('../controllers/blog.controller');
 const auth = require('../middleware/auth.middleware');
 const router = express.Router();
 
+// ✅ Only logged-in users can create blogs
 router.post('/blogs/create', auth, createBlog);
+
+// 🔓 Public blog access by subdomain
 router.get('/blogs/:subdomain', getBlogBySubdomain);
 
 module.exports = router;
