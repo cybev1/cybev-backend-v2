ba3e334662243c0474f8e094245fdf42c828a42f
cybev1 <cybev.io@gmail.com>
2025-05-26 04:34:59 -0700

    Add files via upload

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  const token = req.headers['authorization'];
+module.exports = function (req, res, next) {
+  const token = req.headers.authorization;
+
   if (!token) return res.status(401).json({ message: 'No token provided' });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'cybev');
+    req.userId = decoded.userId;
     next();
   } catch (err) {
-    res.status(403).json({ message: 'Invalid token' });
+    return res.status(403).json({ message: 'Invalid or expired token' });
   }
 };
