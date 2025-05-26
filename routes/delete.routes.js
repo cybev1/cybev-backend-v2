3faed25e298c0df145d93f0bab4bac1692550410
cybev1 <cybev.io@gmail.com>
2025-05-26 05:19:25 -0700

    Update delete.routes.js

--- routes/delete.routes.js
+++ routes/delete.routes.js
@@ -3,6 +3,7 @@ const { deleteBlog } = require('../controllers/blog.controller');
 const auth = require('../middleware/auth.middleware');
 const router = express.Router();
 
+// ✅ Only the owner should delete a blog
 router.delete('/blogs/:id', auth, deleteBlog);
 
 module.exports = router;
