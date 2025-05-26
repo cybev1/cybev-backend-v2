80279f2f087b9edccb18d3615674f31046aa7eda
cybev1 <cybev.io@gmail.com>
2025-05-26 05:34:41 -0700

    Add files via upload

---
+++ controllers/me.controller.js
@@ -0,0 +1,12 @@
+const User = require('../models/user.model');
+
+exports.getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
