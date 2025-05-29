0a1050ac0707727b5bc893b1fef27dfdfdfb4886
cybev1 <cybev.io@gmail.com>
2025-05-29 00:41:53 -0700

    Add files via upload

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,9 +1,32 @@
+
 const express = require('express');
 const router = express.Router();
-const { createPost } = require('../controllers/post.controller');
 const auth = require('../middleware/auth.middleware');
+const {
+  createPost,
+  getUserPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  mintPost,
+  boostPost,
+  pinPost,
+  schedulePost,
+  sharePost
+} = require('../controllers/post.controller');
 
-// ✅ Only logged-in users can create posts
+// Core Routes
 router.post('/create', auth, createPost);
+router.get('/user', auth, getUserPosts);
+router.get('/:id', auth, getPostById);
+router.put('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+
+// Extra Functional Routes
+router.post('/:id/mint', auth, mintPost);
+router.post('/:id/boost', auth, boostPost);
+router.post('/:id/pin', auth, pinPost);
+router.post('/:id/schedule', auth, schedulePost);
+router.post('/:id/share', auth, sharePost);
 
 module.exports = router;
