6795ec8b7fd90df3842f058e7cc284e6594eeb1a
cybev1 <cybev.io@gmail.com>
2025-05-29 05:48:25 -0700

    Add files via upload

---
+++ controllers/post.controller.js
@@ -0,0 +1,34 @@
+
+const Post = require('../models/post.model');
+
+exports.createPost = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const {
+      title, content, category, niche, tags,
+      image, video, status, scheduledDate,
+      isPinned, isBoosted, shareToTimeline
+    } = req.body;
+
+    const post = new Post({
+      title,
+      content,
+      category,
+      niche,
+      tags,
+      image,
+      video,
+      status,
+      scheduledDate,
+      isPinned,
+      isBoosted,
+      shareToTimeline,
+      userId
+    });
+
+    await post.save();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
