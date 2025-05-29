a65e4e207ded38a7ec4ac484798f7b64e5e5cc61
cybev1 <cybev.io@gmail.com>
2025-05-29 03:14:25 -0700

    Add files via upload

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,107 +1,32 @@
-
 const Post = require('../models/post.model');
 
-// Create post
 exports.createPost = async (req, res) => {
   try {
-    const newPost = new Post({ ...req.body, userId: req.user.userId });
-    const saved = await newPost.save();
-    res.status(201).json(saved);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+    const userId = req.user.userId;
+    const {
+      title, content, category, niche, tags,
+      image, video, status, scheduledDate,
+      isPinned, isBoosted, shareToTimeline
+    } = req.body;
 
-// Get user's posts
-exports.getUserPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({ userId: req.user.userId });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+    const post = new Post({
+      title,
+      content,
+      category,
+      niche,
+      tags,
+      image,
+      video,
+      status,
+      scheduledDate,
+      isPinned,
+      isBoosted,
+      shareToTimeline,
+      userId
+    });
 
-// Get a single post by ID
-exports.getPostById = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) return res.status(404).json({ message: 'Post not found' });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Update post
-exports.updatePost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Delete post
-exports.deletePost = async (req, res) => {
-  try {
-    await Post.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Post deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Mint
-exports.mintPost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, { isMinted: true }, { new: true });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Boost
-exports.boostPost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, { isBoosted: true }, { new: true });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Pin
-exports.pinPost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, { isPinned: true }, { new: true });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Schedule
-exports.schedulePost = async (req, res) => {
-  try {
-    const { date } = req.body;
-    const post = await Post.findByIdAndUpdate(req.params.id, {
-      isScheduled: true,
-      scheduledDate: new Date(date)
-    }, { new: true });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Share
-exports.sharePost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, { sharedToTimeline: true }, { new: true });
-    res.json(post);
+    await post.save();
+    res.status(201).json(post);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
