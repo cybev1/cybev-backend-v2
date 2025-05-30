5707960a6f58f658e7706280bcfe21647bb5f5f2
cybev1 <cybev.io@gmail.com>
2025-05-30 09:01:31 +0000

    Add files via upload

--- routes/me.routes.js
+++ routes/me.routes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { getMe } = require('../controllers/me.controller');
-const auth = require('../middleware/auth.middleware');
-const router = express.Router();
 
-router.get('/auth/me', auth, getMe);
+const express = require("express");
+const router = express.Router();
+const meController = require("../controllers/me.controller");
+const verifyToken = require("../middleware/auth.middleware");
+
+router.get("/me", verifyToken, meController.getMe);
 
 module.exports = router;
