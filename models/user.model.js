524c49bcde80c3673c2ade4c9f7d9b3015f12ea0
cybev1 <cybev.io@gmail.com>
2025-05-30 08:00:07 +0000

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  referrer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  username: { type: String, unique: true },
+  email: { type: String, unique: true },
+  passwordHash: String,
+  walletAddress: String,
+  referredBy: String,
+  referralCode: String,
+  profileImage: String,
+  bio: String,
+  interests: [String],
+  features: {
+    hasBlog: { type: Boolean, default: true },
+    hasTimeline: { type: Boolean, default: true },
+    hasNFT: { type: Boolean, default: true },
+    isCMSAdmin: { type: Boolean, default: false },
+    isSuperBlogger: { type: Boolean, default: false },
+    hasUtilityAccess: { type: Boolean, default: true },
+    canUseAI: { type: Boolean, default: false }
+  }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", UserSchema);
