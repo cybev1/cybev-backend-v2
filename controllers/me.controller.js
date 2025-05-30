4993c276eaa7c38df84f2b63c809564b8ce4d808
cybev1 <cybev.io@gmail.com>
2025-05-30 09:00:33 +0000

    Add files via upload

--- controllers/me.controller.js
+++ controllers/me.controller.js
@@ -1,12 +1,13 @@
-const User = require('../models/user.model');
+
+const User = require("../models/user.model");
 
 exports.getMe = async (req, res) => {
   try {
-    const user = await User.findById(req.userId).select('-password');
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch user", error });
   }
 };
