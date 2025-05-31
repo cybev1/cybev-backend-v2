7ec5554e8e7323ea1adf7205ac0682e28335b9b3
cybev1 <cybev.io@gmail.com>
2025-05-31 11:28:48 +0000

    Add files via upload

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
+// Only use the working route for now
 router.post("/register", authController.register);
-router.post("/login", authController.login);
+
+// Commented out temporarily to prevent server crash
+// router.post("/login", authController.login);
 
 module.exports = router;
